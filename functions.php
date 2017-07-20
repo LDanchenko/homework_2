b2ae19793c3fa1be57038af3209e172e639472c6
LDanchenko <2l-u-b-o-v@ukr.net>
2017-07-20 11:04:27 +0300

    выполнено - 2 задание

--- functions.php
+++ functions.php
@@ -14,4 +14,67 @@ function task1($arr, $f)
     return $str;
 }
 
+function task2($arr, $str1)
+{
+
+    echo "<br>";
+    echo 'Массив: ';
+    print_r($arr);
+    echo "<br>";
+    echo 'Операция ' .$str1;
+    echo '<br>';
+    if (empty($arr) or empty($str1)) {
+
+        echo 'Не задана одна переменная!';
+    }
+    elseif (empty($arr) and empty($str1)){
+       // echo 'Не заданы обе переменные!';
+    }
+    else {
+        $b = $arr[0];
+        echo 'Результат = ';
+        switch ($str1) {
+            case ("+"):
+                for ($i = 1; $i < count($arr); $i++) {
+                    $b += $arr[$i];
+                }
+                echo $b;
+                break;
+            case ("-"):
+                for ($i = 1; $i < count($arr); $i++) {
+                    $b -= $arr[$i];
+                }
+                echo $b;
+                break;
+            case ("*"):
+                for ($i = 1; $i < count($arr); $i++) {
+                    $b = $b * $arr[$i];
+                }
+                echo $b;
+                break;
+            case ("/"):
+                for ($i = 1; $i < count($arr); $i++) {
+                    $b = $b / $arr[$i];
+                }
+                echo $b;
+                break;
+            case ("%"):
+                for ($i = 1; $i < count($arr); $i++) {
+                    $b = $b % $arr[$i];
+                }
+                echo $b;
+                break;
+            case ("**"):
+                for ($i = 1; $i < count($arr); $i++) {
+                    $b = $b ** $arr[$i];
+                }
+                echo $b;
+                break;
+            default:
+                echo "Нет такой арифметической операции!";
+                break;
+        }
+    }
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,44 @@
 <?php
 require "functions.php";
-echo "<p> Задание №1 </p>";
+
+echo "<p> <b> Задание №1 </b> </p>";
 $a = false;
 $mas = array("foo", "bar", "hallo", "world");
 $t = task1($mas, $a);
 echo $t;
+
+echo "<p> <b> Задание №2 </b> </p>";
+$arithmetic = "+";
+$b = array(6, 4 , 2);
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = "-";
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = "*";
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = "/";
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = "%";
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = "**";
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = null;
+task2($b, $arithmetic);
+echo '<br>';
+
+$arithmetic = "+";
+$b = null;
+task2($b, $arithmetic);
+echo '<br>';
 ?>
